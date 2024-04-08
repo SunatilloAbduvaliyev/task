f1368e9ddd6eb6054964975dfb6f31c148c575f0
Ali <alisherurinov@68@gmail.com>
2024-04-08 17:25:01 +0500

    ishimni tugatdim!

---
+++ lib/utils/app_fonts.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+import 'app_colors.dart';
+
+
+
+class AppTextStyle {
+  //Poppins
+  static const TextStyle poppinsBold = TextStyle(
+    fontFamily: "Poppins",
+    fontWeight: FontWeight.w200,
+    fontSize: 14,
+    color: AppColors.c_2A3256,
+  );
+  static const TextStyle condensedLight = TextStyle(
+    fontFamily: "Poppins",
+    fontWeight: FontWeight.w300,
+    fontSize: 14,
+    color: AppColors.c_2A3256,
+  );
+  static const TextStyle condensedRegular = TextStyle(
+    fontFamily: "Poppins",
+    fontWeight: FontWeight.w400,
+    fontSize: 16,
+    color: AppColors.c_2A3256,
+  );
+  static const TextStyle condensedMedium = TextStyle(
+    fontFamily: "Poppins",
+    fontWeight: FontWeight.w500,
+    fontSize: 16,
+    color: AppColors.c_2A3256,
+  );
+
+  static const TextStyle interSemiBold = TextStyle(
+    fontFamily: "Poppins",
+    fontWeight: FontWeight.w600,
+    fontSize: 16,
+    color: AppColors.c_2A3256,
+  );
+}
